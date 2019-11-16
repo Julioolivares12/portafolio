c6397ec011f3834364a53182d692340c51aeff0c
julio <julioolivares90@gmail.com>
2019-11-16 12:06:38 -0600

    cambios

--- js/app.js
+++ js/app.js
@@ -6,3 +6,21 @@ function mostrarejercicio(url, t) {
     img.setAttribute('src', url)
 
 }
+
+function teoriaCapitulo1(title) {
+    var app = document.getElementById('app');
+    if (app.hasChildNodes()) {
+        var nodeHijo = app.getElementsByTagName('img')
+
+
+        document.getElementById('mostrarNombreEjercicio').innerHTML = title
+
+    } else {
+        app
+    }
+}
+
+function cambiarIFrame(url) {
+    var iframe = document.getElementById('miPaginas');
+    iframe.setAttribute('src', url)
+}
--- js/main.js
+++ js/main.js
@@ -1,29 +1,3 @@
-function miMenu(event) {
-    var lis = document.getElementsByClassName('subMenu')
-    for (let index = 0; index < lis.length; index++) {
-        const element = lis[index];
-        element.className = 'menuMostrar'
-    }
-}
-//window.onload = init;
-
-function init() {
-    document.querySelectorAll('.subMenu')[0].addEventListener('onClick', () => {
-        var estado = document.querySelector(".subMenu > li").style.display
-        if (estado == "none") {
-            document.querySelector(".submenu > ul").style.display = "block";
-        } else {
-            document.querySelector(".submenu > ul").style.display = "none";
-        }
-    });
-    document.querySelector('.subMenu > li').style.display = "none";
-}
-
-function esconderMenu(event) {
-    var estado = document.querySelector('.subMenu > li').style.display
-    if (estado == "none") {
-        document.querySelector('.subMenu  li').style.display = "list-item";
-    } else {
-        document.querySelector('.subMenu  li').style.display = "none";
-    }
-}
+const app = new Vue({
+    el: '#app',
+})
